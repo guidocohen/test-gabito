aea0357b65478d710c135a4e0cfe1657059b1e1d
Gabriel Magallan <gabriel.magallan@outlook.es>
2023-04-22 17:23:35 -0300

    segundo commit

--- probando.js
+++ probando.js
@@ -1,15 +1,16 @@
 const findTwoBeers = (beersJSON, targetJSON) => {
     const lookup = {};
+    const arr = [];
     const beers = JSON.parse(beersJSON);
     const target = parseInt(targetJSON);
     const beersEnumerated = beers.entries(); // Entries se debe llamar luego de parsear al tipo Array (dentro de la func. findTwoBeers)
 
-    for (const [index, value] of beersEnumerated) {
+    for (const [i, value] of beersEnumerated) {
         const diff = target - value;
         if (diff in lookup) {
-            return [lookup[diff], index];
+            return [lookup[diff], i];
         }
-        lookup[value] = index;
+        lookup[value] = i;
     };
 
     return [];
